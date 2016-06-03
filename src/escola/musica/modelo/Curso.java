e434c1c8089c7e64342d9ce33c18ebbaad54cf73
jpetm <jeanerzinger@gmail.com>
2016-06-03 05:04:21 -0300

    asdasdasd
    asdasd

---
+++ src/escola/musica/modelo/Curso.java
@@ -0,0 +1,69 @@
+package escola.musica.modelo;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Curso {
+
+	private Integer id;
+	private String nome;
+	private String descricao;
+	private double duracao = 1;
+	private TipoCurso tipo;
+	private Date dataCriacao;
+
+	public Date getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(Date dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+	@Id
+	@GeneratedValue
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public double getDuracao() {
+		return duracao;
+	}
+
+	public void setDuracao(double duracao) {
+		this.duracao = duracao;
+	}
+
+	public TipoCurso getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(TipoCurso tipo) {
+		this.tipo = tipo;
+	}
+
+}
